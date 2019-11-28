dc3128ff3831d93f33a6f2d54b5c3bce334be232
AsterixHc <daniel@justesen.biz>
2019-11-28 10:31:24 +0100

    Update
    Updated Characters and Player classes with fields and methods and updated interface IPlayable with methods but not fields

--- ExamenProjectReal/ExamenProjectReal/IPlayable.cs
+++ ExamenProjectReal/ExamenProjectReal/IPlayable.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace ExamenProjectReal
 {
     interface IPlayable
     {
+
+        void Flee();
+
+        void UseItem(Item item);
+
+        void LevelUp();
+
     }
 }
